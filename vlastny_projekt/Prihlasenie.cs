74314d99f917ca26a5a0089e28c139d08d60ef61
MichalSaroch <michal.saroch@centrum.sk>
2021-06-12 11:39:14 +0200

    Pridanie otvaranie okna registracie cez prihlasenie a pripojenie sa na SQL server

--- vlastny_projekt/Prihlasenie.cs
+++ vlastny_projekt/Prihlasenie.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace vlastny_projekt
 {
@@ -19,12 +21,25 @@ namespace vlastny_projekt
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            //this.Hide();
+            var oknoRegistracia = new Registracia();
+            oknoRegistracia.Location = this.Location;
+            oknoRegistracia.StartPosition = FormStartPosition.Manual;
+            oknoRegistracia.FormClosing += delegate { 
+                this.Show();
+                this.Location = oknoRegistracia.Location;
+            };
+            oknoRegistracia.Show();
+            this.Hide();
         }
 
         private void buttonPrihlas_Click(object sender, EventArgs e)
         {
-
+            string connetionString;
+            connetionString = @"Server=.\SQL_PIB;Database=PIB;Trusted_Connection=True;";
+            SqlConnection cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
         }
     }
 }
